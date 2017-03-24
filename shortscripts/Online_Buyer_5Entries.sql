eb6a46d2daa2b6dea21e84b6e49033af9ceb912e
Jalen Johnson <jjohnson101j@gmail.com>
2017-03-24 13:11:30 -0600

    Add files via upload

---
+++ shortscripts/Online_Buyer_5Entries.sql
@@ -0,0 +1,10 @@
+INSERT INTO `online_buyer` (`id_buyer`,`email`,`name`,`address`,`city`,`state`,`zip`) 
+VALUES (1,"euismod23@debot.com","Head, Holly L.","144 Leo. Av.","Cleveland","Ohio","92355");
+INSERT INTO `online_buyer` (`id_buyer`,`email`,`name`,`address`,`city`,`state`,`zip`) 
+VALUES(2,"velit45@msn.com","Lawrence, Kaden P.","Ap #323-1207 Vivamus Road","Dupont","Georgia","61285");
+INSERT INTO `online_buyer` (`id_buyer`,`email`,`name`,`address`,`city`,`state`,`zip`) 
+VALUES(3,"eros47@hotmail.com","Cunningham, Mikayla G.","151-2742 Vitae Av.","Chicago","Illinois","38327");
+INSERT INTO `online_buyer` (`id_buyer`,`email`,`name`,`address`,`city`,`state`,`zip`) 
+VALUES(4,"lightning17754@gmail.com","Lawson, Reece Z.","P.O. Box 840, 8020 Morbi Av.","Luback","Texas","68496")
+INSERT INTO `online_buyer` (`id_buyer`,`email`,`name`,`address`,`city`,`state`,`zip`) 
+VALUES(5,"magnis456@gmail.com","Newton, Lucy R.","P.O. Box 824, 3376 Quis Av.","Tuson","Arizona","X1S 1Y4");
---
+++ shortscripts/location_5Entries.sql
@@ -0,0 +1,10 @@
+INSERT INTO `location` (`phone`,`address`,`city`,`state`,`zip`,`hours`) 
+VALUES ("(198) 897-0729","529 Ante. St.","Nappa Gully","California","26177","8am-10pm");
+INSERT INTO `location` (`phone`,`address`,`city`,`state`,`zip`,`hours`) 
+VALUES("(456) 128-1921","5018 Vel Rd.","Vermont Town","Vermont","81816","8am-9pm")
+INSERT INTO `location` (`phone`,`address`,`city`,`state`,`zip`,`hours`) 
+VALUES("(431) 964-0042","4455 Phasellus St.","Clovis","New Mexico","22440","9am-9pm");
+INSERT INTO `location` (`phone`,`address`,`city`,`state`,`zip`,`hours`) 
+VALUES("(558) 895-2391","5264 Adipiscing Street","Scottsdale","Arizona","68587","7am-7pm");
+INSERT INTO `location` (`phone`,`address`,`city`,`state`,`zip`,`hours`) 
+VALUES("(879) 349-6433","9853 Dolor, St.","Springer","Nevada","82435","Ignacia");
