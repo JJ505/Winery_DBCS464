055a743f748bc2c202fe03b413724e7f4cad4421
Jalen Johnson <jsho20@unm.edu>
2017-04-14 20:32:42 -0600

    adding mark files

---
+++ createIndex.sql
@@ -0,0 +1,15 @@
+Create Index hire_date_idx
+On Employee (startDate);
+
+--DROP INDEX hire_date_idx ON Employee;
+
+Create Index store_phone_idx
+On Store (phone);
+
+--DROP INDEX store_phone_idx ON Store;
+
+Create Index lead_name_idx
+On Online_Lead(name);
+
+--DROP INDEX lead_name_idx ON Online_Lead;
+
