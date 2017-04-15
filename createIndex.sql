0cafaa552aae9c2c7da165c0faaaaff13e34a643
Jalen S. Johnson <cybosaur@cs.unm.edu>
2017-04-15 01:59:15 -0600

    fixed all queries to be syntactically correct and run in mysql

--- createIndex.sql
+++ createIndex.sql
@@ -1,15 +1,15 @@
 Create Index hire_date_idx
-On Employee (startDate);
+On employee (startdate);
 
 --DROP INDEX hire_date_idx ON Employee;
 
 Create Index store_phone_idx
-On Store (phone);
+On store (phone);
 
 --DROP INDEX store_phone_idx ON Store;
 
 Create Index lead_name_idx
-On Online_Lead(name);
+On online_lead(name);
 
 --DROP INDEX lead_name_idx ON Online_Lead;
 
--- queriesAndUpdates.sql
+++ queriesAndUpdates.sql
@@ -12,73 +12,72 @@
 
 -- 10 Queries
 -- 3 general queries
-Select *
+Select A.*
 From (Select *
       From wine
-      Where price >= 100)
+      Where price >= 100) AS A
 Limit 100;
 
-Select *
+Select B.*
 From (Select name, alc_cert
       From sommelier as S, employee as E
-      Where S.id_emp = E.id_emp)
+      Where S.id_emp = E.id_emp) AS B
 Limit 100;
 
-Select *
+Select C.*
 From(Select Type, vintage, food_name
      From wine as W, food as F
-     Where W.price >= F.price And W.price<= (F.price +10) And W.price < 90)
+     Where W.price >= F.price And W.price<= (F.price +10) And W.price < 90) AS C
 Limit 100;
 
 -- 2 join queries related with 3 tables
 Select *
-From(Select name, salary as Yearly_Income  
-     From (manager inner Join employee on manager.id_emp = employee.id_emp)))
+From ((Select name, salary as Yearly_Income  
+     From (manager inner Join employee on manager.id_emp = employee.id_emp)) 
 	union
 	(Select name, hourly_pay * 40 * 52 as Yearly_Income
-	 From(sommelier inner Join employee on sommelier.id_emp = employee.id_emp))
+	 From(sommelier inner Join employee on sommelier.id_emp = employee.id_emp))) AS C
 Limit 100;
 
-Select *
-From(Select online_buyer.name, store.name, location.address
-     From (online_buyer Inner join store on online_buyer.state = store.state) 
-          Inner Join location on store.state = location.state)
+Select online_buyer.name, store.name, location.address
+     From (online_buyer Inner join store on online_buyer.state = store.state
+            INNER JOIN location on store.state = location.state) 
 Limit 100;
  
 -- 1 union query
-Select *
-From(Select city, state
-     From (Select city, state From employee) 
+Select B.*
+From(Select A.city, A.state
+     From (Select city, state From employee
            union 
-          (Select city, state From location)
-     where city Like '%polis%')
+          Select city, state From location) AS A
+     where city Like '%polis%') AS B
 Limit 100;
 
 -- 1 group by query
-Select *
-From(Select name, Count(email) as CNT
-     From online_lead
-     Group by name Having CNT >= 3)
+Select B.*
+From(Select A.name, Count(A.email) as CNT
+     From online_lead AS A
+     Group by A.name Having CNT >= 3) AS B
 Limit 100;
 
 -- 1 order by query
-Select *
-From(Select id_emp, startdate
-	 From employee
-	 Order By startdate)
+Select B.*
+From(Select A.id_emp, A.startdate
+	 From employee AS A
+	 Order By startdate) AS B
 Limit 100;
 
 -- 1 distinct query
-Select *
-From(Select distinct Type, vintage
-     From wine
-     Where price <100)
+Select A.*
+From(Select distinct B.type, B.vintage
+     From wine AS B
+     Where price <100) AS A
 Limit 100;
 
 -- 1 aggregate
-Select *
-From(Select AVG(salary) as salary_Average
-     From manager)
+Select A.*
+From(Select AVG(B.salary) as salary_Average
+     From manager AS B)AS A
 Limit 100;
 
 -- 6 sql data modifications
